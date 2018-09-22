ff87c56995a48134fdf743627ff699d6e69dc289
harry1003 <b05901059@ntu.edu.tw>
2018-09-22 19:35:34 +0800

    hw1-1

--- hw1/hw1-1.py
+++ hw1/hw1-1.py
@@ -3,51 +3,131 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense,Activation
 
-x = np.linspace(0.00001,1,9999)
-y = np.sin(5*np.pi*x)/(5*np.pi*x)
+def generate_data_1():
+    x = np.linspace(0.00001,1,9999)
+    y = np.sin(5*np.pi*x)/(5*np.pi*x)
 
-index=list(range(0,9999))
-np.random.seed(1024)
-np.random.shuffle(index)
+    index=list(range(0,9999))
+    np.random.seed(1024)
+    np.random.shuffle(index)
 
-x = x[index]
-y = y[index]
+    x_u = x[index]
+    y_u = y[index]
 
-x_train = x[0:9000]
-y_train = y[0:9000]
+    x_train = x_u[0:9000]
+    y_train = y_u[0:9000]
 
-x_test = x[9000:]
-y_test = y[9000:]
+    x_test = x_u[9000:]
+    y_test = y_u[9000:]
+    return (x_train,y_train,x_test,y_test,x,y)
+  
+def generate_data_2():
+    x = np.linspace(0.00001,1,9999)
+    y = np.sign( np.sin(5*np.pi*x) )
 
-#### generate model ####
-print('model build')
+    index=list(range(0,9999))
+    np.random.seed(1024)
+    np.random.shuffle(index)
 
-model = Sequential()
-model.add( Dense( 5 , input_shape=(1,) ) )
-model.add( Activation('relu') ) 
+    x_u = x[index]
+    y_u = y[index]
 
-model.add( Dense(5) )
-model.add( Activation('relu') ) 
+    x_train = x_u[0:9000]
+    y_train = y_u[0:9000]
 
-model.add( Dense(5) )
-model.add( Activation('relu') ) 
+    x_test = x_u[9000:]
+    y_test = y_u[9000:]
+    return (x_train,y_train,x_test,y_test,x,y)
 
-model.add( Dense(5) )
-model.add( Activation('relu') ) 
+def model_generator_1():
+    print('model_1 build')
+    model = Sequential()
+    model.add( Dense( 5 , input_shape=(1,) ) )
+    model.add( Activation('relu') ) 
 
-model.add( Dense(1) )
-model.compile( loss = 'mse', optimizer='adam',metrics=['accuracy'])
-#### generate model ####
+    model.add( Dense(5) )
+    model.add( Activation('relu') ) 
 
-#### train ####
-print('start training')
-history = model.fit( x_train, y_train, batch_size=100, epochs=100, verbose=1, validation_data=(x_test,y_test) )
- 
-#### save model ####
-print('save file')
-model.save('hw1-1a.h5')
+    model.add( Dense(5) )
+    model.add( Activation('relu') ) 
+
+    model.add( Dense(5) )
+    model.add( Activation('relu') ) 
+    
+    model.add( Dense(5) )
+    model.add( Activation('relu') ) 
+    
+    model.add( Dense(1) )
+    model.compile( loss = 'mse', optimizer='adam')
+    print(model.summary())
+    return model
+  
+def model_generator_2():
+    print('model_3 build')
+    model = Sequential()
+    model.add( Dense( 10 , input_shape=(1,) ) )
+    model.add( Activation('relu') ) 
+
+    model.add( Dense(6) )
+    model.add( Activation('relu') ) 
+    
+    model.add( Dense(6) )
+    model.add( Activation('relu') ) 
+    
+    model.add( Dense(1) )
+    model.compile( loss = 'mse', optimizer='adam')
+    print(model.summary())
+    return model  
+  
+def model_generator_3():
+    print('model_2 build')
+    model = Sequential()
+    model.add( Dense( 45 , input_shape=(1,) ) )
+    model.add( Activation('relu') ) 
+    
+    model.add( Dense(1) )
+    model.compile( loss = 'mse', optimizer='adam')
+    print(model.summary())
+    return model
+
+
     
 import matplotlib.pyplot as plt          
-#### print accuracy ####
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss']) 
+
+def train(x_train,y_train,x_test,y_test,x,y,epochs):
+    
+    model_1 = model_generator_1()
+    model_2 = model_generator_2()
+    model_3 = model_generator_3()
+    
+    history_1 = model_1.fit( x_train, y_train, batch_size=100, epochs=epochs, verbose=1, validation_data=(x_test,y_test) )
+    history_2 = model_2.fit( x_train, y_train, batch_size=100, epochs=epochs, verbose=1, validation_data=(x_test,y_test) )
+    history_3 = model_3.fit( x_train, y_train, batch_size=100, epochs=epochs, verbose=1, validation_data=(x_test,y_test) )
+    
+    
+    plt.plot(history_1.history['val_loss'], color = 'blue')
+    plt.plot(history_2.history['val_loss'], color = 'brown')
+    plt.plot(history_3.history['val_loss'], color = 'green')
+    
+    plt.show()
+    
+    y_1 = model_1.predict(x)
+    y_2 = model_2.predict(x)
+    y_3 = model_3.predict(x)
+    
+    plt.plot(y)
+    plt.plot(y_1, color = 'blue')
+    plt.plot(y_2, color = 'brown')
+    plt.plot(y_3, color = 'green')
+    
+    plt.show()
+    
+    
+def main():   
+    x_train,y_train,x_test,y_test,x,y = generate_data_1()
+    train(x_train,y_train,x_test,y_test,x,y,50)
+    x_train,y_train,x_test,y_test,x,y = generate_data_2()
+    train(x_train,y_train,x_test,y_test,x,y,150)
+    
+if __name__ == '__main__':
+    main()
