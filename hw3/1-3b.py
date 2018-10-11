d03b96ffcb6586754ae817b2865569eefe024aa8
alphay <arthuryaucs@gmail.com>
2018-10-11 20:32:48 +0800

    1. fixed the bug

--- hw3/1-3b.py
+++ hw3/1-3b.py
@@ -28,22 +28,23 @@ class ModelConfig():
     def __init__(self, config):
         self.filter_1_size = config[0]
         self.num_of_filter_in_layer_1 = config[1]
-        self.filter_2_size = config[2]
-        self.num_of_filter_in_layer_2 = config[3]
+        # self.filter_2_size = config[2]
+        # self.num_of_filter_in_layer_2 = config[3]
 
 def model_config_generation():
     filter_1_size = [(3, 3), (4, 4), (5, 5)]
-    num_of_filter_in_layer_1 = [2, 4, 8, 16, 32]
-    filter_2_size = [(3, 3), (4, 4), (5, 5)]
-    num_of_filter_in_layer_2 = [2, 4, 8, 16, 32]
+    num_of_filter_in_layer_1 = [2, 4, 8, 16, 32, 64]
+    # filter_2_size = [(3, 3), (4, 4), (5, 5)]
+    # num_of_filter_in_layer_2 = [2, 4, 8, 16, 32]
 
-    # filter_1_size = [(3, 3)]
-    # num_of_filter_in_layer_1 = [16]
+    # filter_1_size = [(3, 3), (4, 4)]
+    # num_of_filter_in_layer_1 = [8, 16, 24]
     # filter_2_size = [(3, 3), (5, 5)]
     # num_of_filter_in_layer_2 = [16]
 
     config_setting = [filter_1_size, num_of_filter_in_layer_1,
-        filter_2_size, num_of_filter_in_layer_2]
+        # filter_2_size, num_of_filter_in_layer_2
+        ]
     
     config_set = list(itertools.product(*config_setting))
     config_set = list(map(ModelConfig, config_set))
@@ -55,8 +56,8 @@ def model_generator(model_config, activation='relu'):
     input = Input(shape=(28,28,1,), name='input')
     layer = Conv2D(model_config.num_of_filter_in_layer_1, model_config.filter_1_size, activation=activation, name='layer1')(input)
     layer = MaxPooling2D(pool_size=(2, 2))(layer)
-    layer = Conv2D(model_config.num_of_filter_in_layer_2, model_config.filter_2_size, activation=activation, name='layer2')(input)
-    layer = MaxPooling2D(pool_size=(2, 2))(layer)
+    # layer = Conv2D(model_config.num_of_filter_in_layer_2, model_config.filter_2_size, activation=activation, name='layer2')(input)
+    # layer = MaxPooling2D(pool_size=(2, 2))(layer)
     layer = Flatten()(layer)
     end = Dense(10, activation='softmax', name='end')(layer)     
     model = Model(input=input, output=end)
@@ -71,10 +72,9 @@ def train_and_get_loss_acc_and_param_size(model, x_train, y_train, x_test, y_tes
                             verbose=1,
                             validation_data=(x_test, y_test))
 
-    return model.count_params(), res.history['acc'], res.history['loss'], res.history['val_acc'], res.history['val_loss']
+    return model.count_params(), res.history['acc'][-1], res.history['loss'][-1], res.history['val_acc'][-1], res.history['val_loss'][-1]
 
 def plot_result(x, y1, y2, title, label1, label2, x_tile, y_title, fig_name):
-    colors = (0,0,0)
     area = np.pi*3
     
     # Plot
@@ -101,12 +101,20 @@ def main():
     for config in config_set:
         model = model_generator(config)
         param_size, acc, loss, val_acc, val_loss = train_and_get_loss_acc_and_param_size(
-            model, x_train, y_train, x_test, y_test, epochs=20)
+            model, x_train, y_train, x_test, y_test, epochs=5)
+        print()
         model_param_size.append(param_size)
+        print(param_size)
         model_train_acc.append(acc)
+        print(acc)
         model_train_loss.append(loss)
+        print(loss)
         model_test_acc.append(val_acc)
+        print(val_acc)
         model_test_loss.append(val_loss)
+        print(val_loss)
+
+    
 
     plot_result(model_param_size, model_train_loss, model_test_loss,
         'MNIST Model Loss','train_loss', 'test_loss', 'number of parameters', 'loss', '3b_loss')
